45c59694adb33a28023456a654f6199a8649657e
ajiways <ajiwayswc@gmail.com>
2022-03-17 08:36:01 +0700

    Added base config files

---
+++ src/misc/config.ts
@@ -0,0 +1,12 @@
+import { config } from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+config();
+
+const configService = new ConfigService();
+
+export const PG_HOST = configService.get<string>('PG_HOST');
+export const PG_USER = configService.get<string>('PG_USER');
+export const PG_PASSWORD = configService.get<string>('PG_PASSWORD');
+export const PG_DATABASE = configService.get<string>('PG_DATABASE');
+export const PG_PORT = configService.get<number>('PG_PORT');
+export const APP_PORT = configService.get<number>('APP_PORT');
