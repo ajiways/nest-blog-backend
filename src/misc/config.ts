0f7268b83ae7290ca7cd04966084a29dbb651f04
ajiways <ajiwayswc@gmail.com>
2022-03-17 22:40:31 +0700

    added secrets to config

--- src/misc/config.ts
+++ src/misc/config.ts
@@ -10,3 +10,5 @@ export const PG_PASSWORD = configService.get<string>('PG_PASSWORD');
 export const PG_DATABASE = configService.get<string>('PG_DATABASE');
 export const PG_PORT = configService.get<number>('PG_PORT');
 export const APP_PORT = configService.get<number>('APP_PORT');
+export const REFRESH_SECRET = configService.get<string>('REFRESH_SECRET');
+export const ACCESS_SECRET = configService.get<string>('ACCESS_SECRET');
